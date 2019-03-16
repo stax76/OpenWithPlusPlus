a5ba895c6d76a66266ddb22f25f32e1daae8ea94
Frank Skare <frank.skare.de@gmail.com>
2019-03-16 02:06:23 +0100

    -

--- release.ps1
+++ release.ps1
@@ -3,7 +3,6 @@ $exePath = $scriptDir + "\OpenWithPPGUI\bin\OpenWithPPGUI.exe"
 $version = [Diagnostics.FileVersionInfo]::GetVersionInfo($exePath).FileVersion
 $desktopDir = [Environment]::GetFolderPath("Desktop")
 $targetDir = $desktopDir + "\OpenWithPP-" + $version
-aaa
 Copy-Item $scriptDir\OpenWithPPGUI\bin $targetDir -Recurse -Exclude *.lib, *.exp
 Copy-Item $scriptDir\README.md $targetDir\README.md
 $7zPath = "C:\Program Files\7-Zip\7z.exe"
