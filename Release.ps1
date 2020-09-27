8e6d3239956cec6f307825023cf01b79f8910dab
Frank Skare <frank.skare.de@gmail.com>
2020-09-27 15:26:36 +0200

    new screenshot

--- Release.ps1
+++ Release.ps1
@@ -1,12 +1,14 @@
 
-$ScriptDir = Split-Path -Path $PSCommandPath -Parent
-$ExePath = $ScriptDir + '\OpenWithPPGUI\bin\OpenWithPPGUI.exe'
-$Version = [Diagnostics.FileVersionInfo]::GetVersionInfo($ExePath).FileVersion
+$ExePath    = $PSScriptRoot + '\OpenWithPPGUI\bin\OpenWithPPGUI.exe'
+$Version    = [Diagnostics.FileVersionInfo]::GetVersionInfo($ExePath).FileVersion
 $DesktopDir = [Environment]::GetFolderPath('Desktop')
-$TargetDir = $DesktopDir + '\OpenWithPP-' + $Version
-Copy-Item $ScriptDir\OpenWithPPGUI\bin $TargetDir -Recurse
+$TargetDir  = $DesktopDir + '\OpenWithPP-' + $Version
+
+Copy-Item $PSScriptRoot\OpenWithPPGUI\bin $TargetDir -Recurse
+
 Remove-Item $TargetDir\OpenWithPPShellExtension.pdb -ErrorAction Ignore
 Remove-Item $TargetDir\OpenWithPPShellExtension.lib -ErrorAction Ignore
 Remove-Item $TargetDir\OpenWithPPShellExtension.exp -ErrorAction Ignore
 Remove-Item $TargetDir\OpenWithPPShellExtension.ilk -ErrorAction Ignore
+
 & 'C:\Program Files\7-Zip\7z.exe' a -t7z -mx9 "$TargetDir.7z" -r "$TargetDir\*"
